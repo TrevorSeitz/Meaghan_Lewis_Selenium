511b3bf10c60fe995a5e2f102454a1c7199c0ad9
Trevor Seitz <trevinla@gmail.com>
2022-02-14 16:30:41 -0500

    Initial Commit

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/tseitz/Desktop/LinkedIn_Learning/chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("121 elmcroft road, Rochester, NY");
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+}
